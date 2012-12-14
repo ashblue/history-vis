381d5c985c9aed38a63c191baa355ebb74f49d78
Ash Blue <ash@blueashes.com>
2012-12-13 16:43:29 -0800

    Added tween library and integrated it into the framework to use the delta

--- public/app/models/sphere.js
+++ public/app/models/sphere.js
@@ -1,10 +1,18 @@
 define(
-    ['controllers/scene'],
+    [
+        'controllers/scene',
+        'lib/tween'
+    ],
 
     function (
-        scene
+        scene,
+        Tween
     ) {
-        var DEFAULT_COLOR = 0xCC0000;
+        var DEFAULT_COLOR = 0x999999;
+
+        var _tweenColor = new Tween(0, 1, 1000, 'linear');
+
+        var _tweenSize = new Tween(0, 1, 1000, 'linear');
 
         /**
          * @todo Break up logic into storage
@@ -41,11 +49,15 @@ define(
 
         Sphere.prototype.update = function () {
             // Set color
-            this.material.color.setRGB(1, 0.5, 0.5);
+            //this.material.color.setRGB(1, 0.5, 0.5);
 
             // Set size
-            this.mesh.scale.y = 1.2;
-            this.mesh.scale.x = 1.2;
+            //this.mesh.scale.y = 1.2;
+            //this.mesh.scale.x = 1.2;
+
+        };
+
+        Sphere.prototype.addText = function () {
 
         };
 
